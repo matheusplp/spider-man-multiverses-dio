99939cce9780d7c3f44b5a5b52587ed9409c069e
Matheus PLP <matheus.plp@gmail.com>
2024-12-16 00:21:05 -0300

    Adding Carousel animation to Homepage

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -23,3 +23,19 @@ function handleMouseLeave()
   this.classList.remove('s-card--hovered');
   document.body.id = ``;
 }
+
+function selectCarouselItem(selectedButtonElement)
+{
+  const selectedItem = selectedButtonElement.id;
+  const carousel = document.querySelector('.s-cards-carousel');
+  const transform = carousel.style.transform;
+  const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
+  const rotateYDeg = -120 * (Number(selectedItem) - 1);
+  const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
+
+  carousel.style.transform = newTransform;
+
+  const activeButtonElement = document.querySelector('.s-controller__button--active');
+  activeButtonElement.classList.remove('s-controller__button--active');
+  selectedButtonElement.classList.add('s-controller__button--active');
+}
